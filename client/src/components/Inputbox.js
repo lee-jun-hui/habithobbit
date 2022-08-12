7cf1a4fc6d7e1eb52a2acaecc69bd538bc3a2335
enirevlow <foowinlin@gmail.com>
2022-08-12 12:28:07 +0800

    initial commit fe structure

---
+++ client/src/components/Inputbox.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Inputbox = () => {
+  return (
+    <View>
+      <Text>Inputbox</Text>
+    </View>
+  );
+};
+
+export default Inputbox;
