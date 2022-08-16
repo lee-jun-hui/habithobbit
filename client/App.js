f05add11e92e73b2045f828a566519b9bec1d18a
loisiling <silingloi@hotmail.com>
2022-08-16 19:43:46 +0800

    version 0.1 dashboard

--- client/App.js
+++ client/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Dashboard from './src/screens/Dashboard';
+import { NavigationContainer } from '@react-navigation/native';
+import DashboardTab from './src/routes/DashboardTab'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <DashboardTab/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ client/src/routes/DashboardTab.js
@@ -0,0 +1,52 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DashboardScreen from "../screens/Dashboard";
+import Icon from 'react-native-vector-icons/AntDesign'
+import { StyleSheet, Text, View} from "react-native";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator 
+        screenOptions={{
+            headerShown:false,
+            tabBarStyle:{
+                backgroundColor: "white",
+                borderTopWidth: 0,
+                justifyContent: "center",
+                alignItems: "center"
+            },
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: "black",
+            tabBarInactiveTintColor: "grey"
+            }}>
+            <Tab.Screen name="Home" component={DashboardScreen} options={{
+                tabBarIcon:({focused, color})=>(
+                    <Icon name="home" size={26} color={color}></Icon>
+                ),
+            }}/>
+            <Tab.Screen name="Team" component={DashboardScreen} options={{
+                tabBarIcon:({focused, color})=>(
+                    <Icon name="team" size={26} color={color}></Icon>
+                ),
+            }}/>
+            <Tab.Screen name="Plus" component={DashboardScreen} options={{
+                tabBarIcon:({focused, color})=>(
+                    <Icon name="pluscircle" size={49} color={"#4E53BA"} style={{top: 0}}></Icon>
+                ),
+            }}/>
+            <Tab.Screen name="Schedule" component={DashboardScreen} options={{
+                tabBarIcon:({focused, color})=>(
+                    <Icon name="calendar" size={26} color={color}></Icon>
+                ),
+            }}/>
+            <Tab.Screen name="User" component={DashboardScreen} options={{
+                tabBarIcon:({focused, color})=>(
+                    <Icon name="user" size={26} color={color}></Icon>
+                ),
+            }}/>
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs
