9256de043407c3e16148b3255b395aa627e28d22
Ivy Nah <ivy@Ivys-MacBook-Pro.local>
2022-08-17 17:06:13 +0800

    added create habits

--- client/App.js
+++ client/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CreateHabit from './src/screens/CreateHabit';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CreateHabit />
     </View>
   );
 }
