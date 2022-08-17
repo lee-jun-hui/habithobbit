95d1640e4f6725fb538a4c790080df081b4534de
Ivy Nah <ivy@Ivys-MacBook-Pro.local>
2022-08-17 19:11:42 +0800

    working on api

--- src/screens/CreateHabit.js
+++ src/screens/CreateHabit.js
@@ -1,10 +1,28 @@
 import { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
+import axiosConn from '../api/config';
 
 const CreateHabit = () => {
-  const [date, setDate] = useState(new Date())
-
+  const [habitData, setHabitData] = useState(
+    {
+      name: "",
+      description: "",
+      frequency:
+        {
+          repeat: 'daily',
+          mon: true,
+          tues: true,
+          wed: true,
+          thurs: true,
+          fri: true,
+          sat: true,
+          sun: true,
+        },
+      endDate: "",
+    },
+  );
+  //const [test, setTest] = useState({})
   const [habit, setHabit] = useState(null);
   const [habitDesc, setHabitDesc] = useState(null);
   
@@ -26,9 +44,42 @@ const CreateHabit = () => {
     {label: 'Sunday', value: 'sunday'},
   ]);
 
-  const createHabit = () => {
+  const createHabit = async () => {
+    console.log(habit);
+    console.log(habitDesc);
     console.log(freqValue);
     console.log(dayValue);
+    //console.log(habitData);
+    //setTest(prevTest => ({...prevTest, name: habit}));
+
+    setHabitData((prevHabit) => ({
+      ...prevHabit,
+      name: habit,
+      description: habitDesc,
+      frequency:
+        {
+          repeat: freqValue,
+          mon: dayValue.includes('monday'),
+          tues: dayValue.includes('tuesday'),
+          wed: dayValue.includes('wednesday'),
+          thurs: dayValue.includes('thursday'),
+          fri: dayValue.includes('friday'),
+          sat: dayValue.includes('saturday'),
+          sun: dayValue.includes('sunday'),
+        },
+    }))
+    setHabitData((state) => {
+      console.log(state);
+      
+      return state;
+    });
+    try {
+      //const url = '/api/v1/habits'
+      const response = axiosConn.post('/api/v1/habits', habitData)
+      console.log(response);
+    } catch (error) {
+      console.log(error.response.data);
+    }
     return (
         alert('Habit created')
     )
@@ -63,13 +114,13 @@ const CreateHabit = () => {
         <TextInput
             style={styles.inputTxt}
             placeholder='Enter a Habit'
-            onChange={setHabit}
+            onChangeText={(value) => {setHabit(value)}}
             value={habit} />
       <Text style={styles.headerTxt}>Habit Description</Text>
         <TextInput
             style={styles.inputTxt}
             placeholder='Enter a Description'
-            onChange={setHabitDesc}
+            onChangeText={(value) => {setHabitDesc(value)}}
             value={habitDesc} />
       <Text style={styles.headerTxt}>Repeat</Text>
         <DropDownPicker
@@ -104,10 +155,9 @@ const CreateHabit = () => {
             maxHeight={300}
             zIndex={2000}
         />
-      <Text style={styles.headerTxt}>End Date</Text>
       <TouchableOpacity
             style={styles.button}
-            onPress={createHabit}>
+            onPress={() => {createHabit()}}>
         <Text style={styles.buttonTxt}>Create</Text>
       </TouchableOpacity>
     </View>
