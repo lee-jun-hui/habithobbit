708af71da669626e553a8aad813d09a33d32e738
enirevlow <foowinlin@gmail.com>
2022-08-18 17:47:26 +0800

    update kbav styles

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,22 +1,22 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { DefaultTheme } from 'react-native-paper'
-import { getStatusBarHeight } from 'react-native-status-bar-height'
+import { DefaultTheme } from "react-native-paper";
+import { getStatusBarHeight } from "react-native-status-bar-height";
 
 export const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    text: '#FFFFFF',
-    primary: '#4E53BA',
-    secondary: '#110580',
-    tertiary: '#E8E8F7',
-    subtext: '#868AE0',
-    error: '#f13a59',
+    text: "#FFFFFF",
+    primary: "#4E53BA",
+    secondary: "#110580",
+    tertiary: "#E8E8F7",
+    subtext: "#868AE0",
+    error: "#f13a59",
   },
-}
+};
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export const styles = StyleSheet.create({
   container: {
@@ -27,33 +27,33 @@ export const styles = StyleSheet.create({
   },
   welcomebg: {
     flex: 1,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     width: windowWidth,
     height: windowHeight,
   },
   welcomecontainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
-    width: '100%',
+    width: "100%",
     maxWidth: 340,
-    marginTop: '110%'
+    marginTop: "110%",
   },
   welcomeheader: {
     fontSize: 32,
     color: theme.colors.text,
-    fontFamily: 'roboto-bold',
+    fontFamily: "roboto-bold",
     paddingVertical: 12,
   },
   welcometext: {
     fontSize: 12,
     lineHeight: 16,
     color: theme.colors.text,
-    textAlign: 'center',
-    fontFamily: 'roboto-light',
-    marginBottom: '50%'
+    textAlign: "center",
+    fontFamily: "roboto-light",
+    marginBottom: "50%",
   },
   button: {
     width: 160,
@@ -62,120 +62,122 @@ export const styles = StyleSheet.create({
     backgroundColor: theme.colors.primary,
   },
   buttontext: {
-    fontFamily: 'roboto-regular',
+    fontFamily: "roboto-regular",
     fontSize: 16,
     lineHeight: 16,
   },
   onboardcontainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
     width: windowWidth,
-    paddingBottom: '15%',
+    paddingBottom: "15%",
     height: windowHeight,
-    backgroundColor: theme.colors.text
+    backgroundColor: theme.colors.text,
   },
   onboard1: {
     flex: 1,
-    width: '80%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '30%',
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "30%",
   },
   openingheader: {
     fontSize: 32,
     color: theme.colors.secondary,
-    fontFamily: 'roboto-bold',
+    fontFamily: "roboto-bold",
     paddingVertical: 12,
   },
   openingtext: {
     fontSize: 12,
     lineHeight: 16,
     color: theme.colors.subtext,
-    textAlign: 'center',
-    fontFamily: 'roboto-light',
-    marginBottom: '20%',
+    textAlign: "center",
+    fontFamily: "roboto-light",
+    marginBottom: "20%",
     paddingHorizontal: 45,
   },
   onboard2: {
     flex: 1,
-    width: '90%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '30%',
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "30%",
   },
   onboard3: {
     flex: 1,
-    width: '80%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '30%',
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "30%",
   },
   roundcontainer: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    marginBottom: '20%'
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: "20%",
   },
   darkround: {
     backgroundColor: theme.colors.primary,
     width: 8,
     height: 8,
     borderRadius: 50,
-    marginRight: 9
-  },  
+    marginRight: 9,
+  },
   lightround: {
     backgroundColor: theme.colors.tertiary,
     width: 8,
     height: 8,
     borderRadius: 50,
-    marginRight: 9
-  },  
+    marginRight: 9,
+  },
   registercontainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "flex-start",
     padding: 10,
     width: windowWidth,
-    paddingTop: '5%',
-    paddingBottom: '10%',
+    paddingTop: "5%",
+    paddingBottom: "10%",
     height: windowHeight,
     backgroundColor: theme.colors.text,
+    // backgroundColor: "blue",
   },
   registerrow: {
-    flexDirection: 'row',
-    paddingTop: '5%'
+    flexDirection: "row",
+    paddingTop: "5%",
   },
   link: {
-    fontFamily: 'roboto-bold',
+    fontFamily: "roboto-bold",
     color: theme.colors.primary,
   },
   register: {
     flex: 1,
-    width: '70%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: '70%',
+    resizeMode: "contain",
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "20%",
   },
   registertext: {
-    fontFamily: 'roboto-regular',
+    fontFamily: "roboto-regular",
     color: theme.colors.subtext,
   },
   logincontainer: {
-    width: '80%',
+    width: "80%",
     height: 50,
     marginVertical: 12,
   },
   reginputcontainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: windowWidth,
-    paddingBottom: '30%',
+    paddingBottom: "30%",
     height: windowHeight,
   },
   logininput: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
   },
   logindescription: {
     fontSize: 13,
@@ -188,7 +190,7 @@ export const styles = StyleSheet.create({
     paddingTop: 8,
   },
   backbtncontainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 10 + getStatusBarHeight(),
     left: 4,
   },
@@ -197,8 +199,8 @@ export const styles = StyleSheet.create({
     height: 35,
   },
   forgotPassword: {
-    width: '100%',
-    alignItems: 'flex-end',
+    width: "100%",
+    alignItems: "flex-end",
     marginBottom: 24,
   },
   forgot: {
@@ -207,17 +209,28 @@ export const styles = StyleSheet.create({
   },
   login: {
     flex: 1,
-    width: '70%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: '75%',
+    width: "70%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "75%",
   },
   logininputcontainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: windowWidth,
-    paddingBottom: '45%',
+    // paddingBottom: "45%",
     height: windowHeight,
+  },
+  keyboardavoiding: {
+    flex: 1,
+    padding: 10,
+    width: "100%",
+    // width: windowWidth,
+    // height: windowHeight,
+    maxWidth: 400,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
