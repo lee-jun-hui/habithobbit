cb3bd482d3d943fb09f5dfb03300ea4ed2f0eefe
enirevlow <foowinlin@gmail.com>
2022-08-19 13:33:16 +0800

    update captions

--- src/screens/Onboarding1.js
+++ src/screens/Onboarding1.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import Header from '../components/loginHeader'
-import Button from '../components/loginButton'
-import Paragraph from '../components/loginParagraph'
+import React from "react";
+import Header from "../components/loginHeader";
+import Button from "../components/loginButton";
+import Paragraph from "../components/loginParagraph";
 
-import { Image, View } from 'react-native'
-import { styles } from '../styles/styles'
-
+import { Image, View } from "react-native";
+import { styles } from "../styles/styles";
 
 export default function Onboarding1({ navigation }) {
   return (
     <View style={styles.onboardcontainer}>
-      <Image source={require('../assets/onboarding1.png')} style={styles.onboard1} resizeMode='contain'/>
+      <Image
+        source={require("../assets/onboarding1.png")}
+        style={styles.onboard1}
+        resizeMode="contain"
+      />
       <Header>Create Habits</Header>
       <Paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Don't be lazy! Be like a hobbit and stock up some habits!
       </Paragraph>
       <View style={styles.roundcontainer}>
-        <View style={styles.darkround} /><View style={styles.lightround} /><View style={styles.lightround} />
+        <View style={styles.darkround} />
+        <View style={styles.lightround} />
+        <View style={styles.lightround} />
       </View>
       <Button
         mode="contained"
-        onPress={() => navigation.navigate('Onboarding2')}
+        onPress={() => navigation.navigate("Onboarding2")}
       >
         Next
       </Button>
     </View>
-)}
-
+  );
+}
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import Background from '../components/loginBackground'
-import Logo from '../components/loginLogo'
-import Header from '../components/welcomeHeader'
-import Button from '../components/loginButton'
-import Paragraph from '../components/welcomeParagraph'
+import React from "react";
+import Background from "../components/loginBackground";
+import Logo from "../components/loginLogo";
+import Header from "../components/welcomeHeader";
+import Button from "../components/loginButton";
+import Paragraph from "../components/welcomeParagraph";
 
 export default function Welcome({ navigation }) {
   return (
     <Background>
       <Header>Habits Tracker</Header>
-      <Paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </Paragraph>
+      <Paragraph>Knock! knock! Who's there? WYD? HBU?</Paragraph>
       <Button
         mode="contained"
-        onPress={() => navigation.navigate('Onboarding1')}
+        onPress={() => navigation.navigate("Onboarding1")}
       >
         Let's go
       </Button>
     </Background>
-  )
+  );
 }
